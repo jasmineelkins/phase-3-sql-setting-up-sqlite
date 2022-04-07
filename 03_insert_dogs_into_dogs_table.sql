606c44a7eacf1b95657a7a8c100588d8c8b5844f
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-04-07 15:21:20 -0400

    Completed through SQL Queries lesson, practicing data manipulation in pets.db

---
+++ 03_insert_dogs_into_dogs_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO dogs (name, age, breed) VALUES ('Midas', 4, 'Doofus');
+INSERT INTO dogs (name, age, breed) VALUES ('Cecil', 2, 'Mutt');
+INSERT INTO dogs (name, age, breed) VALUES ('Cookie', 3, 'Fluffball');
+INSERT INTO dogs (name, age, breed) VALUES ('Karl', 8, 'Smiling Teddy Bear');
+
+
+
